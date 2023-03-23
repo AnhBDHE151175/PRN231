07bdb63fda318f30a269f083604437e5567c6309
ANHBDHE151175 <ANHBDHE151175@fpt.edu.vn>
2023-03-24 00:43:02 +0700

    update code

---
+++ PRN231_UI/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PRN231_UI.Models;
+using System.Diagnostics;
+
+namespace PRN231_UI.Controllers
+{
+    public class LoginController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            TempData["IsLogin"] = false;
+            return View();
+        }
+        public IActionResult Login()
+        {
+            HttpContext.Session.SetString("IsLogin", "true");
+
+            TempData["IsLogin"] = true;
+            return Redirect("/Home/Index");
+        }
+
+    }
+}
